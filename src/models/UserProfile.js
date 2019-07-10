ebfe82f3bb20f6ff857c1a60e144b68ab8fb7909
Brent Abruzese <brent.abruzese@gmail.com>
2019-07-09 21:28:20 -0400

    added some validation

--- src/models/UserProfile.js
+++ src/models/UserProfile.js
@@ -1,21 +1,40 @@
 // This will be the user profile model for our Mongo DB
-
 const mongoose = require('mongoose');
-
-mongoose.connect('mongodb://localhost/rpg', {useNewUrlParser: true} )
-
+mongoose.connect('mongodb://localhost/rpg', { useNewUrlParser: true } )
 const db = mongoose.connection;
 
 db.on('error', console.error.bind(console, 'connection error:'));
 db.once('open', function() {
-  // we're connected!
 });
 
-const userProfile = new mongoose.Schema({
-    username: String,
-    password: String,
-    email: String,
-    date: { type: Date, default: Date.now }
+const userProfileSchema = new mongoose.Schema({
+    username: {
+        type: String,
+        trim: true,
+        required: "A username is required!"
+    },
+    password: {
+        type: String,
+        trim: true,
+        required: "Password is Required",
+            validate: [
+                function(input) {
+                return input.length >= 6;
+                },
+                "Password should be longer." 
+                ]
+    },
+    email: {
+        type: String,
+        unique: true,
+        match: [/.+@.+\..+/, "A valid e-mail request is required!"]
+      },
+    userCreated: {
+        type: Date,
+        default: Date.now
+      },
 })
+
+const userProfile = mongoose.model("User", userProfileSchema)
 
 module.exports = userProfile;
