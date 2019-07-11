7e1148caf8cf56a497d7886a8dbefa9140034ab8
Dan Correa <dancorrea3@gmail.com>
2019-07-10 20:29:58 -0400

    Created files/folders for heroku deployment

--- src/App.js
+++ src/App.js
@@ -2,6 +2,14 @@ import React, { Component } from 'react';
 import './App.css';
 import LoginScreen from './Loginscreen';
 
+// import Navbar from './Components/Layout/Navbar';
+// import { BrowserRouter as Router, Route } from 'react-router-dom';
+// import Footer from './Components/Layout/Footer';
+// import Home from './Components/Layout/Home';
+// import Social from './Components/social/Social';
+
+// THE WEBSITE ALSO IMPORTED ALL OF THESE AS WELL. I CREATED THE PROPER FILES FOR ROUTING PURPOSES, BUT THEYRE EMPTY.
+  
 class App extends Component {
   constructor(props){
     super(props);
@@ -9,6 +17,7 @@ class App extends Component {
       loginPage:[],
       uploadScreen:[]
     }
+    this.connecToServer = this.connecToServer.bind(this);
   }
   componentWillMount(){
     var loginPage =[];
@@ -16,6 +25,12 @@ class App extends Component {
     this.setState({
       loginPage:loginPage
     })
+  }
+  connecToServer() {
+    fetch('/');
+  }
+  componentDidMount() {
+        this.connecToServer();
   }
   render() {
     return (
@@ -28,3 +43,15 @@ class App extends Component {
 }
 
 export default App;
+
+// render() {
+//   return (<Router>
+//     <div className="container">
+//       <Navbar />
+//       <Route exact path="/" component={Home} />
+//       <Route exact path="/social" component={Social} />
+//       <Footer />
+//     </div>
+//   </Router>);
+// }
+// }
