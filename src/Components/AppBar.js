4b4b1d02e22ed7ff80fb8d1f4ae78ba82443d35a
Som Ramnani <som.ramnani@gmail.com>
2019-07-11 19:57:23 -0500

    changed background image

--- src/Components/AppBar.js
+++ src/Components/AppBar.js
@@ -27,9 +27,9 @@ export default function ButtonAppBar() {
     <div className={classes.root}>
       <AppBar position="static">
         <Toolbar>
-          <IconButton edge="start" className={classes.menuButton} color="inherit" aria-label="Menu">
+          {/* <IconButton edge="start" className={classes.menuButton} color="inherit" aria-label="Menu">
             <MenuIcon />
-          </IconButton>
+          </IconButton> */}
           <Typography variant="h6" className={classes.title}>
             THE CURSE OF BRENT
           </Typography>
