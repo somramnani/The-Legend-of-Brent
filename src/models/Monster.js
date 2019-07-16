0e53786b160ac89574b6ed0e38a5b263aead1df6
Dan Correa <dancorrea3@gmail.com>
2019-07-15 22:44:32 -0400

    Created Character object in Monster.js

---
+++ src/models/Monster.js
@@ -0,0 +1,35 @@
+import Character from "../Screens/Characters";
+Character = {
+
+    0: {
+        'name': "Fallen Angel",
+        'attack power': 25,
+        'attack speed': 10
+    },
+
+    1: {
+        'name': "Demon Soldier",
+        'attack power': 15,
+        'attack speed': 20
+    },
+    2: {
+        'name': "Oni Lord",
+        'attack power': 30,
+        'attack speed': 9
+    },
+    3: {
+        'name': "Oni Soldier",
+        'attack power': 12,
+        'attack speed': 30
+    },
+    4: {
+        'name': "Fiend Lord",
+        'attack power': 40,
+        'attack speed': 9
+    },
+    5: {
+        'name': "Fiend Soldier",
+        'attack power': 18,
+        'attack speed': 19
+    },
+}
