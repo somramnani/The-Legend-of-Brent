faf4c43e71e095f239ce2c32d02727a9593c434a
Brent Abruzese <brent.abruzese@gmail.com>
2019-07-18 20:51:07 -0400

    tweaked server.js

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const bodyParser = require('body-parser');
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 5000;
 const app = express();
 
 app.use(bodyParser.urlencoded({ extended: false }));
