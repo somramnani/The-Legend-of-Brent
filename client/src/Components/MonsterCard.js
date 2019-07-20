7ed192f5a807213d6e3e706fafc097771a05ec55
Som Ramnani <som.ramnani@gmail.com>
2019-07-20 14:00:07 -0500

    added info from the players api to link to the characters page so it displays

--- client/src/Components/MonsterCard.js
+++ client/src/Components/MonsterCard.js
@@ -1,15 +1,19 @@
 import React from "react";
+import Box from "@material-ui/core/Box";
+import Grid from "@material-ui/core/Grid";
 import Card from "@material-ui/core/Card";
+
 import CardMedia from "@material-ui/core/CardMedia";
 import CardContent from "@material-ui/core/CardContent";
 import CardActionArea from "@material-ui/core/CardActionArea";
 import { createStyles, makeStyles } from "@material-ui/core/styles";
 import Typography from "@material-ui/core/Typography";
+import { getThemeProps } from "@material-ui/styles";
 
 const useStyles = makeStyles(
   createStyles({
     card: {
-      maxWidth: 250,
+      width: 250,
       margin: 30
     },
     media: {
@@ -18,14 +22,14 @@ const useStyles = makeStyles(
   })
 );
 
-export default function MonsterCard() {
+export default function MonsterCard(props) {
   const classes = useStyles();
   const [values, setValues] = React.useState({
-    name: "",
-    img: "",
+    name: props.name,
+    img: props.img,
     title: "",
-    attackSpeed: "",
-    attackPower: ""
+    attackSpeed: props.attackSpeed,
+    attackPower: props.attackPower
   });
 
   return (
@@ -37,12 +41,14 @@ export default function MonsterCard() {
           title={values.title}
         />
         <CardContent>
-          <Typography gutterBottom variant="h4" component="h4">
+          <Typography gutterBottom variant="h5" component="h5">
             {values.name}
           </Typography>
           <Typography variant="body2" component="p" color="textSecondary">
-            Stats: Attack Speed: {values.attackSpeed}
-            Attack Power: {values.attackPower}
+            <Grid direction="column">
+              <Typography> Attack Speed: {values.attackSpeed}</Typography>
+              <Typography> Attack Power: {values.attackPower}</Typography>
+            </Grid>
           </Typography>
         </CardContent>
       </CardActionArea>
