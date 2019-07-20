0a63fe01c392995ebe0dc6ea05150a70e359dc2b
Brent Abruzese <brent.abruzese@gmail.com>
2019-07-20 12:53:38 -0400

    created battle screen and added db user profile

---
+++ client/src/Screens/Battle.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import { Container, Box, Button } from 'react';
+
+class BattleScreen extends Component {
+    smallAttack() {
+        //logic to pass to buttons here
+    }
+
+    bigAttack() {
+        //logic to pass to buttons here
+    }
+
+}
+
+export default BattleScreen;
