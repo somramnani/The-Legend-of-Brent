8a919995f8fe569b5d2841703a46b9a0811e4250
Som Ramnani <som.ramnani@gmail.com>
2019-07-23 20:22:14 -0500

    fixed app.js junk

--- client/src/App.js
+++ client/src/App.js
@@ -1,44 +1,28 @@
 import MonsterScreen from "../src/Screens/MonsterSelect";
-import React, { Component } from 'react';
-import Button from '@material-ui/core/Button'
-import LogIn from './Components/LogIn';
-import SignUp from './Components/SignUp';
-import TitleBar from './Components/TitleBar';
-import CharSelect from './Screens/CharSelect';
-import AuthHelperMethods from './Components/_AuthHelper';
-import withAuth from './Components/withAuth';
-
+import React, { Component } from "react";
+import Button from "@material-ui/core/Button";
+import LogIn from "./Components/LogIn";
+import SignUp from "./Components/SignUp";
+import TitleBar from "./Components/TitleBar";
+import CharSelect from "./Screens/CharSelect";
+import AuthHelperMethods from "./Components/_AuthHelper";
+import withAuth from "./Components/withAuth";
 
 class App extends Component {
-
-
   Auth = new AuthHelperMethods();
 
   _handleLogout = () => {
-    this.Auth.logout()
-    alert('you have successfully logged out')
+    this.Auth.logout();
+    alert("you have successfully logged out");
     // this.props.history.replace('/user/login');
-  }
+  };
 
   render(props) {
     return (
       <>
-
-      <TitleBar />
-        <LogIn />
-        <Button
-        onClick={this._handleLogout} 
-        variant="contained"
-        color="primary"
-        > 
-          Log Out 
-        </Button>
-
         <TitleBar />
 
         <MonsterScreen />
-
-
       </>
     );
   }
