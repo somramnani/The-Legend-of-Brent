100f3a29014790fa4c596248e521753b6b474d2c
Som Ramnani <som.ramnani@gmail.com>
2019-07-24 15:52:30 -0500

    edited the login box

--- client/src/App.js
+++ client/src/App.js
@@ -1,14 +1,14 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
-import Button from '@material-ui/core/Button'
-import LogInScreen from './Screens/LogIn'
-import TitleBar from './Components/TitleBar';
-import CharSelect from './Screens/CharSelect';
-import AuthHelperMethods from './Components/_AuthHelper';
-import GameStore from './context/context'
-import withAuth from './Components/withAuth'
-import LogIn from './Screens/LogIn'
-import SignUp from './Components/SignUp'
+import Button from "@material-ui/core/Button";
+import LogInScreen from "./Screens/LogIn";
+import TitleBar from "./Components/TitleBar";
+import CharSelect from "./Screens/CharSelect";
+import AuthHelperMethods from "./Components/_AuthHelper";
+import GameStore from "./context/context";
+import withAuth from "./Components/withAuth";
+import LogIn from "./Screens/LogIn";
+import SignUp from "./Components/SignUp";
 
 import BattleScreen from "./Screens/Battle";
 
@@ -34,34 +34,41 @@ class App extends Component {
 
   render(props) {
     return (
-      <GameStore.Provider
-        value={{
-          globalState: this.state,
-          chooseCharacter: value => this.setState({ character: value })
-        }}
-      >
-        <GameStore.Consumer>
-          {
-            ({
-              globalState,
-              chooseCharacter
-            }) => (
-              <Router>
-              <div>
-                <Switch>
-                  <Route exact path="/" component={LogIn} />
-                  <Route exact path="/login" component={LogIn} />
-                  <Route exact path="/signup" component={SignUp} />
-                </Switch>
-              </div>
-            </Router>
-            )
-          }
+      //To render pages:
+      // 1. Make sure Brents bullshit (Gamestore.Provider) is commented out.
+      // 2. Then Below the <> Type in what pages you want rendered.
+      <>
+        <TitleBar />
+        <LogInScreen />
+      </>
+      // <GameStore.Provider
+      //   value={{
+      //     globalState: this.state,
+      //     chooseCharacter: value => this.setState({ character: value })
+      //   }}
+      // >
+      //   <GameStore.Consumer>
+      //     {
+      //       ({
+      //         globalState,
+      //         chooseCharacter
+      //       }) => (
+      //         <Router>
+      //         <div>
+      //           <Switch>
+      //             <Route exact path="/" component={LogIn} />
+      //             <Route exact path="/login" component={LogIn} />
+      //             <Route exact path="/signup" component={SignUp} />
+      //           </Switch>
+      //         </div>
+      //       </Router>
+      //       )
+      //     }
 
-        </GameStore.Consumer>
-      </GameStore.Provider>
+      //   </GameStore.Consumer>
+      // </GameStore.Provider>
     );
   }
 }
 
-export default withAuth(App);
+export default App;
--- client/src/Components/LogIn.js
+++ client/src/Components/LogIn.js
@@ -1,25 +1,30 @@
-import React, { Component } from 'react';
-import Container from '@material-ui/core/Container';
-import Box from '@material-ui/core/Box';
-import Button from '@material-ui/core/Button';
-import { Typography } from '@material-ui/core';
-import Paper from '@material-ui/core/Paper';
-import TextField from '@material-ui/core/TextField';
-import AuthHelperMethods from '../Components/_AuthHelper'
-import { BrowserRouter as Router, Route, Link, Redirect } from 'react-router-dom'
+import React, { Component } from "react";
+import Container from "@material-ui/core/Container";
+import Box from "@material-ui/core/Box";
+import Button from "@material-ui/core/Button";
+import { Typography } from "@material-ui/core";
+import Paper from "@material-ui/core/Paper";
+import TextField from "@material-ui/core/TextField";
+import AuthHelperMethods from "../Components/_AuthHelper";
+import {
+  BrowserRouter as Router,
+  Route,
+  Link,
+  Redirect
+} from "react-router-dom";
 
 class LogIn extends Component {
   constructor(props) {
-    super(props)
+    super(props);
   }
 
   Auth = new AuthHelperMethods();
 
   state = {
-    username: '',
-    password: '',
-    loginSuccess: false,
-  }
+    username: "",
+    password: "",
+    loginSuccess: false
+  };
 
   changeHandler = event => {
     this.setState({
@@ -29,72 +34,111 @@ class LogIn extends Component {
   };
 
   signUp = () => {
-    return <Typography>Need an account? Sign up here</Typography>
-  }
+    return <Typography>Need an account? Sign up here</Typography>;
+  };
 
   handleFormSubmit = event => {
     event.preventDefault();
     this.Auth.login(this.state.username, this.state.password)
       .then(response => {
-        if(response.status === 400) {
+        if (response.status === 400) {
           return alert("Sorry those credentials don't exist!");
         }
-        this.setState({loginSuccess: true});
+        this.setState({ loginSuccess: true });
       })
       .catch(err => {
         alert(err);
       });
   };
 
-render() {
-  if (this.state.loginSuccess) {
-    return <Redirect to="/CharSelect" />
-  }
-  return(
-  <>
-    <Container maxWidth="sm">
-    <Typography>Sign up to play now!</Typography>
-      <Paper>
-        <Box >
-          <Typography variant="h6" component="h6">
-            Log In Below
+  render() {
+    if (this.state.loginSuccess) {
+      return <Redirect to="/CharSelect" />;
+    }
+    return (
+      <>
+        <Container
+          maxWidth="sm"
+          style={{
+            fontFamily: "Merienda, cursive",
+            transform: "translate(-25em,5em)"
+          }}
+        >
+          <Typography
+            variant="h4"
+            component="h4"
+            style={{
+              color: "white",
+              fontFamily: "Merienda, cursive",
+              paddingBottom: "20px"
+            }}
+          >
+            Log In To Play Now!
           </Typography>
-          <TextField
-            id="outlined-name"
-            label="Name"
-            name="username"
-            onChange={this.changeHandler}
-            margin="normal"
-            variant="outlined"
-          />
-          <TextField
-            id="outlined-password-input"
-            name="password"
-            label="Password"
-            onChange={this.changeHandler}
-            type="password"
-            margin="normal"
-            variant="outlined"
-          />
-        </Box>
-        <Box component="span">
-          <Button 
-            variant="contained" 
-            color = "primary"
-            id = "loginButton"
-            onClick={this.handleFormSubmit}
+          <Paper>
+            <Box style={{ padding: "20px" }}>
+              <Typography
+                variant="h6"
+                component="h6"
+                style={{
+                  fontFamily: "Merienda, cursive"
+                }}
+              >
+                Log In Below
+              </Typography>
+              <TextField
+                id="outlined-name"
+                label="Name"
+                name="username"
+                onChange={this.changeHandler}
+                margin="normal"
+                variant="outlined"
+              />
+              <TextField
+                id="outlined-password-input"
+                name="password"
+                label="Password"
+                onChange={this.changeHandler}
+                type="password"
+                margin="normal"
+                variant="outlined"
+                style={{
+                  paddingLeft: "3px"
+                }}
+              />
+            </Box>
+            <Box
+              component="span"
+              style={{
+                padding: "20px"
+              }}
             >
-            Login
-          </Button>
-            Don't have an account? 
-            <a href="/signup">
-              Sign up here
-            </a>
-        </Box>
-      </Paper>
-    </Container>
-  </>
-  )
+              <Button
+                variant="contained"
+                color="primary"
+                id="loginButton"
+                onClick={this.handleFormSubmit}
+                style={{
+                  fontFamily: "Merienda, cursive"
+                }}
+              >
+                Login
+              </Button>
+              <Typography
+                style={{
+                  paddingTop: "10px",
+                  paddingLeft: "20px",
+                  fontFamily: "Merienda, cursive"
+                }}
+              >
+                Don't have an account?
+                <a href="/signup">Sign up here </a>
+              </Typography>
+            </Box>
+          </Paper>
+        </Container>
+      </>
+    );
   }
 }
 
