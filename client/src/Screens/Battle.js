c1e95e839768e768a31e979944c7438b8c77a707
Som Ramnani <som.ramnani@gmail.com>
2019-07-23 21:00:12 -0500

    fixed battle.js file

--- client/src/Screens/Battle.js
+++ client/src/Screens/Battle.js
@@ -1,62 +1,10 @@
-import React, { Component } from 'react';
-import { Container, Box, Button } from 'react';
-import MonsterCard from '../Components/MonsterCard';
-import Monster from '../data/Monster.json'
-import Player from '../data/Player.json'
+import { Typography } from "@material-ui/core";
+import React, { Component } from "react";
 
 class BattleScreen extends Component {
-  smallAttack() {
-    console.log("small attack was clicked");
-  }
-
-  bigAttack() {
-    console.log("big attack was clicked");
-  }
-
-  specialAttack() {
-    //code for specialAttack
-  }
-
   render() {
-    return (
-      <>
-      <Container>
-        <Box>
-          <MonsterCard>
-          <Player>
-            <Button 
-            onClick={this.smallAttack} >
-              Small Attack
-            </Button>
-            <Button 
-            onClick={this.bigAttack} >
-              Big Attack
-            </Button>
-            <Button 
-            onClick={this.specialAttack} >
-              Special Attack
-            </Button>
-          </Player>
-{/* player is above, monster is below */}
-            <Monster>
-            <Button 
-            onClick={this.smallAttack} >
-              Small Attack
-            </Button>
-            <Button 
-            onClick={this.bigAttack} >
-              Big Attack
-            </Button>
-            <Button 
-            onClick={this.specialAttack} >
-              Special Attack
-            </Button>
-            </Monster>
-          </MonsterCard>
-        </Box>
-      </Container>
-    </>
-    )
+    return <Typography>Hello World</Typography>;
   }
 }
+
 export default BattleScreen;
