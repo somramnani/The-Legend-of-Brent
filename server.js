5f22112326d6141d7246db7fd6b8456ecee19531
Brent Abruzese <brent.abruzese@gmail.com>
2019-08-02 13:03:10 -0400

    heroku deploy take 9000

--- server.js
+++ server.js
@@ -77,10 +77,10 @@ app.post("/user/login", async (req, res) => {
   }
 });
 
-app.get("/CharSelect", jwtMW, (req, res) => {
-  console.log("Web Token Checked.");
-  alert("You are authenticated");
-});
+// app.get("/CharSelect", jwtMW, (req, res) => {
+//   console.log("Web Token Checked.");
+//   alert("You are authenticated");
+// });
 
 app.get("*", (req, res) => {
   res.sendFile(path.join(__dirname, "client", "build", "index.html"));
