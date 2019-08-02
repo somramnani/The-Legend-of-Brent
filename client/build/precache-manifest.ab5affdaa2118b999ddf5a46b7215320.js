09350e46ca16a112fd6d9404477d320cd9d4825e
Brent Abruzese <brent.abruzese@gmail.com>
2019-08-02 13:08:10 -0400

    created production build

---
+++ client/build/precache-manifest.ab5affdaa2118b999ddf5a46b7215320.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "884e903f464af6357962bd1eb6cf186e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "6604877c5fb9c89de919",
+    "url": "/static/css/main.7b18ad73.chunk.css"
+  },
+  {
+    "revision": "775c72c3c7983f11c28a",
+    "url": "/static/js/2.4f730ccd.chunk.js"
+  },
+  {
+    "revision": "6604877c5fb9c89de919",
+    "url": "/static/js/main.0c2e3595.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
