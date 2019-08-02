b6b1f6d34576dc99c1bbc9fa8763e0cf3f47f700
Brent Abruzese <brent.abruzese@gmail.com>
2019-08-02 14:22:20 -0400

    testing

--- server.js
+++ server.js
@@ -84,8 +84,8 @@ app.post("/user/login", async (req, res) => {
 //   alert("You are authenticated");
 // });
 
-app.get("/", (req, res) => {
-  res.sendFile(path.join(__dirname, "client", "build", "index.html"));
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "build", "index.html"));
 });
 
 // if (process.env.NODE_ENV === "production") {
