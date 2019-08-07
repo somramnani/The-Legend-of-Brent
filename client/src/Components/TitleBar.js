5f36a7f80b5fa72e94e83d00aabc104d31f2e992
Brent Abruzese <brent.abruzese@gmail.com>
2019-08-07 12:31:25 -0400

    wrapped App.js with Auth0Provider component

--- client/src/Components/TitleBar.js
+++ client/src/Components/TitleBar.js
@@ -4,8 +4,7 @@ import AppBar from "@material-ui/core/AppBar";
 import Toolbar from "@material-ui/core/Toolbar";
 import Button from "@material-ui/core/Button";
 import Typography from "@material-ui/core/Typography";
-import AuthHelperMethods from "./_AuthHelper";
-import { BrowserRouter as Router, Link, Redirect } from "react-router-dom";
+import { useAuth0 } from '../react-auth0-wrapper'
 
 const useStyles = makeStyles(theme => ({
   root: {
@@ -24,74 +23,52 @@ const useStyles = makeStyles(theme => ({
 
 export default function TitleBar() {
   const classes = useStyles();
-
-  const handleLogout = () => {
-    if (!Auth.loggedIn()) {
-      alert("You are not currently logged in.");
-    } else {
-      Auth.logout();
-      return <Redirect to="/" />;
-    }
-  };
-
-  const Auth = new AuthHelperMethods();
+  const { isAuthenticated, loginWithRedirect, logout } = useAuth0();
 
   return (
     <div className={classes.root}>
       <AppBar className={classes.appbar} position="static">
-        <Toolbar>
-          {/* <IconButton edge="start" className={classes.menuButton} color="inherit" aria-label="Menu">
-          </IconButton> */}
-
-          <Typography
-            variant="h6"
-            className={classes.title}
-            style={{
-              fontFamily: "Merienda, cursive"
-            }}
-          >
-            <Link
-              to="/"
-              color="inherit"
-              style={{
-                textDecoration: "none",
-                color: "white",
-                paddingLeft: "20px"
-              }}
-            >
-              THE LEGEND OF BRENT
-            </Link>
-          </Typography>
-          <Link
-            to="/CharSelect"
-            color="inherit"
-            style={{
-              fontFamily: "Merienda, cursive",
-              textDecoration: "none",
-              color: "white"
-            }}
-            onClick={() => {
-              // this.props.deleteCharacter();
-            }}
-          >
+      <Toolbar>
+        <Typography
+          variant="h6"
+          className={classes.title}
+          style={{
+            fontFamily: "Merienda, cursive"
+          }} >
+        <Link
+          to="/"
+          color="inherit"
+          style={{
+            textDecoration: "none",
+            color: "white",
+            paddingLeft: "20px"
+          }} >
+            THE LEGEND OF BRENT
+        </Link>
+        </Typography>
+        <Link
+          to="/CharSelect"
+          color="inherit"
+          style={{
+            fontFamily: "Merienda, cursive",
+            textDecoration: "none",
+            color: "white"
+          }}
+          onClick={() => {
+        // this.props.deleteCharacter();
+          }} >
             CHARACTER SELECTION
-          </Link>
-          <Link
-            to="/"
-            color="inherit"
-            style={{
-              fontFamily: "Merienda, cursive",
-              textDecoration: "none",
-              color: "white",
-              paddingLeft: "20px"
-            }}
-            onClick={() => {
-              handleLogout();
-            }}
-          >
-            LOGOUT
-          </Link>
-        </Toolbar>
+        </Link>
+        {!isAuthenticated && (
+        <Button
+          onClick={() =>
+            loginWithRedirect({})
+          }>
+          Log in
+        </Button>
+        )}
+        {isAuthenticated && <Button onClick={() => logout()}>Log out</Button>}
+      </Toolbar>
       </AppBar>
     </div>
   );
